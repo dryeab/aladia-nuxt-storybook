d6229230b29c899d646371b2f66565803378616f
Yeabsira Driba <dryeab@gmail.com>
2024-12-04 18:35:18 +0300

    feat: added element plus

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/storybook"],
+  modules: ["@nuxtjs/storybook", "@element-plus/nuxt"],
   css: ["@/assets/css/tailwind.css"],
   postcss: {
     plugins: {
